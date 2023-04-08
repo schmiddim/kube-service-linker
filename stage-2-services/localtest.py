065d487110deb98af4142ca1ba13c0cf8d1e54a5
Michael Schmitt <schmiddim@gmx.at>
2023-04-08 17:28:52 +0200

    wired up, err catching

---
+++ stage-2-services/localtest.py
@@ -0,0 +1,9 @@
+import json
+import logging as log
+from modules.logic import analyze_requirements
+
+log.getLogger().setLevel(log.INFO)
+
+with open('tests/fixtures/post.json') as f:
+    data = json.load(f)
+    analyze_requirements(data)
---
+++ stage-2-services/modules/logic.py
@@ -0,0 +1,33 @@
+import logging
+
+logger = logging.getLogger(__name__)
+
+
+def analyze_requirements(data):
+    logger.info("=== Try to map Services===")
+    if len(data.get('exposed_services')) == 0:
+        logger.info("No exposed services")
+        return
+    if len(data.get("deployments_with_requirements")) == 0:
+        logger.info("No deployments with requirements")
+        return
+
+    for deployment in data.get("deployments_with_requirements"):
+
+        if deployment.get('event') not in ['ADDED', 'MODIFIED']:
+            raise Exception("implement for method", deployment)
+        for service in data.get('exposed_services'):
+
+            if service.get('event') not in ['ADDED', 'MODIFIED']:
+                raise Exception("implement for method", service)
+
+            for label, value in service.get('service').get('labels').items():
+                for requirement in deployment.get("requirements"):
+                    if label == 'decMgmtProvides':
+                        if requirement == value:
+                            logger.info("Depl {} in ns {} requirements {} fulfilled by {} ".format(
+                                deployment.get('deployment').get('name'),
+                                deployment.get('deployment').get('namespace'),
+                                requirement,
+                                label)
+                            )
